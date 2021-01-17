ff1e7b3a2923140a014fc459759b01c200622efe
houngkyung-kim <rkido90@gmail.com>
2021-01-17 10:57:29 +0900

    creating react component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import Potato from "./Potato";
+function Food({ fav }) {
+  return <h1>I love {fav} </h1>;
+}
 
 function App() {
   return (
     <div className="App">
       <h1>Hello!</h1>
-      <Potato />
+      <Food fav="kimchi" />
+      <Food fav="ramen" />
+      <Food fav="samkyupsal" />
+      <Food fav="chukumi" />
     </div>
   );
 }
