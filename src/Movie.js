170c2f57ce8b5f819c50bb94012335026d99e26e
houngkyung-kim <rkido90@gmail.com>
2021-02-13 11:49:40 +0900

    #4.4 Styles

--- src/Movie.js
+++ src/Movie.js
@@ -14,12 +14,12 @@ Movie.propTypes = {
 function Movie({year, title, summary, poster, genres}) {
   return (
     <div className="movie">
-      <img className="poster" src={poster} alt={title} title={title} />
-      <div className="data">
-        <h3 className="title">{title}</h3>
-        <h5 className="year">{year}</h5>
-        <p className="summary">{summary}</p>
-        <ul className="genres">{genres.map((genre, index) => <li key={index} className="genres__genre">{genre}</li>)}</ul>
+      <img className="movie__poster" src={poster} alt={title} title={title} />
+      <div className="movie__data">
+        <h3 className="movie__title">{title}</h3>
+        <h5 className="movie__year">{year}</h5>
+        <p className="movie__summary">{summary}</p>
+        <ul className="movie__genres">{genres.map((genre, index) => <li key={index} className="genres__genre">{genre}</li>)}</ul>
       </div>
     </div>
   );
